bf2eef38dccce2de9c6cc37f4b3a8c6b046da9c8
Albertobarrio <albertobarrio2012@gmail.com>
2022-03-29 10:44:37 -0300

    Agrgando tag

--- Ejercicios/Procesos/ejecutor.py
+++ Ejercicios/Procesos/ejecutor.py
@@ -13,17 +13,31 @@ cli_parser = argparse.ArgumentParser(prog='Ejecutor',
 
 # Agrego los argumentos
 
-cli_parser.add_argument('-c', '--c', action='store',
-                        metavar='command', help='Comando a ejecutar', dest='command', type=str, required=True)
-cli_parser.add_argument('-o', '--o', action='store', metavar='output_file',
-                        help='Almacena la salida del comando ejecutado', dest='output_file')
-cli_parser.add_argument('-l', '--l', action='store', metavar='log_file',
-                        help='Almacena un mesaje o la salida de error', dest='log_file')
+cli_parser.add_argument('-c', 
+                        '--c',
+                        action='store',
+                        metavar='command',
+                        help='Comando a ejecutar', 
+                        dest='command', 
+                        type=str, 
+                        required=True)
+cli_parser.add_argument('-o', 
+                        '--o', 
+                        action='store',
+                        metavar='output_file',
+                        help='Almacena la salida del comando ejecutado', 
+                        dest='output_file')
+cli_parser.add_argument('-l',
+                        '--l', 
+                        action='store', 
+                        metavar='log_file',
+                        help='Almacena un mesaje o la salida de error', 
+                        dest='log_file')
 
 # Ejecutamos el metodo parse_args()
 
 args = cli_parser.parse_args()
 #print(vars(args))
 
-
-process = sp.Popen((args.command).split())
+#sp.Popen([args.command], shell=True)
+#process = sp.Popen((args.command).split())
