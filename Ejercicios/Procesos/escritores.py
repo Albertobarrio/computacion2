3a2ba3aeef7b3c07a150d2fbc49a26806d4acc8f
Albertobarrio <albertobarrio2012@gmail.com>
2022-04-11 19:59:02 -0300

    Fuinciona todo y modo verboso agregado

--- Ejercicios/Procesos/escritores.py
+++ Ejercicios/Procesos/escritores.py
@@ -2,12 +2,6 @@ import argparse
 import os
 import time
 
-def escribir(file_open):
-    file_open.write('H')
-    file_open.flush()
-    time.sleep(2)    
-
-
 cli_parser = argparse.ArgumentParser(prog='Escritor',
                                      description='Crea n procesos hijos que escriben una letra en un archivo',
                                      epilog='Trabajo Practico Obligatorio')
@@ -17,15 +11,15 @@ cli_parser.add_argument('-n',
                         action='store',
                         type=int,
                         required=True,
-                        choices=range(1,28),
+                        choices=range(1, 27),
                         help='Numero de procesos hijos')
 
-# cli_parser.add_argument('-r',
-#                         '--rep',
-#                         action='store',
-#                         type=int,
-#                         required=True,
-#                         help='Cantidad de veces que se debe escribir una letra')
+cli_parser.add_argument('-r',
+                        '--rep',
+                        action='store',
+                        type=int,
+                        required=True,
+                        help='Cantidad de veces que se debe escribir una letra')
 
 cli_parser.add_argument('-f',
                         '--filepath',
@@ -41,19 +35,27 @@ cli_parser.add_argument('-v',
 args = cli_parser.parse_args()
 
 
-file_open = open(args.filepath, 'a+')
+file_open = open(args.filepath, 'w+')
 
 for _ in range(args.numero):
-    print(f'{_} veces')
     if os.fork() == 0:
-        print('Soy el hijo')
-        escribir(file_open)
+        letra = chr(65 + _)
+        for _ in range(args.rep):
+            if args.verboso:
+                print(f'Proceso {os.getpid()} escribiendo letra {letra}')
+            file_open.write(letra)
+            file_open.flush()
+            time.sleep(1)
         os._exit(0)
+
+# El padre espera a que termine los hijos
 os.wait()
 
+# Cerramos el archivo
 file_open.close()
 
-
-
-
-
+# El padre lee el arhivo nuevo y lo muestra por pantalla
+with open(args.filepath, 'r') as new_file:
+    for line in new_file:
+        print(line)
+        # print(line, end='')
